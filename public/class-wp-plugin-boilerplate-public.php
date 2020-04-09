acc01505d9c5fbada56260a4db7b08e73a40d01a
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2020-04-09 23:43:55 +0530

    :art: files enqueued

--- public/class-wp-plugin-boilerplate-public.php
+++ public/class-wp-plugin-boilerplate-public.php
@@ -73,7 +73,7 @@ class Wp_Plugin_Boilerplate_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-plugin-boilerplate-public.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/dist/css/bundle.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Wp_Plugin_Boilerplate_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-plugin-boilerplate-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/dist/css/app.js', array( 'jquery' ), $this->version, false );
 
 	}
 
